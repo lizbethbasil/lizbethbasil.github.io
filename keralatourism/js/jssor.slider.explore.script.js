cc914fd415b65acaadf35c5e7397489381545e1e
Lizbeth <lizbeth.2821@gmail.com>
2022-04-02 20:04:36 +0530

    second commit

--- keralatourism/js/jssor.slider.explore.script.js
+++ keralatourism/js/jssor.slider.explore.script.js
@@ -12,7 +12,7 @@ window.jssor_1_slider_init = function(){
     var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
 
     /*#region responsive code begin*/
-    var MAX_WIDTH = 1320;
+    var MAX_WIDTH = 1240;
 
     function ScaleSlider(){
         var containerElement    =   jssor_1_slider.$Elmt.parentNode;
