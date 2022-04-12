6682885c43151c9cf5bc33468854a1528b4f9a51
Lizbeth <lizbeth.2821@gmail.com>
2022-04-12 23:17:30 +0530

    Update signinValidation.js

--- keralatourism/js/signinValidation.js
+++ keralatourism/js/signinValidation.js
@@ -18,5 +18,5 @@ function validateLogin(){
         document.getElementById("password-status").style.color = "red"
     }
     else
-        window.location = "../index.html";
+        window.location = "..index.html";
 }
