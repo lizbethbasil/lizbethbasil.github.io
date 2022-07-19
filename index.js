aee6c246dae030cce35ec06ac08e475b7866f5ac
Lizbeth <lizbeth.2821@gmail.com>
2022-07-19 21:56:45 +0530

    updates

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const Bookdata = require("./src/model/BookData");
+
+// added
+const path = require("path");
+app.use(express.static('./dist/library-app-frontend'));
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/library-app-frontend/index.html'));
+});
+// added
+
+const app = new express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// API
+app.get('/api/booklist', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+    Bookdata.find()
+        .then(function (books) {
+            res.send(books);
+        })
+});
+
+app.post('/api/newbook', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+    console.log(req.body);
+    var book = {
+        bookCode: req.body.item.bookCode,
+        bookName: req.body.item.bookName,
+        authorName: req.body.item.authorName,
+        description: req.body.item.description,
+        rating: req.body.item.rating,
+        imageURL: req.body.item.imageURL
+    }
+
+    console.log("New Book Added");
+    var books = new Bookdata(book);
+    books.save();
+})
+
+app.listen(5000, () => {
+    console.log("Server up in Port# 5000 ");
+});
